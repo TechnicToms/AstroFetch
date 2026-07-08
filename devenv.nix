8769581faffabc70d15a6e765e06bcff5026fff0
Mirza Arnaut <mirza.arnaut45@gmail.com>
2026-07-08 16:38:37 +0200

    chore: add pre-commit hooks for ruff

--- devenv.nix
+++ devenv.nix
@@ -3,11 +3,22 @@
   ...
 }:
 {
+  packages = with pkgs; [
+    prek
+    uv
+    ruff
+    ty
+  ];
+
   enterShell = ''
     if [ ! -L "$DEVENV_ROOT/.venv" ]; then
         ln -s "$DEVENV_STATE/venv/" "$DEVENV_ROOT/.venv"
     fi
+    prek install --install-hooks --overwrite
+    # pre-commit install --install-hooks -f
   '';
+
+  # pre-commit.package = pkgs.prek;
 
   languages.python = {
     enable = true;
